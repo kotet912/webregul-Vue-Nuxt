0fd935749cb06e76ffd85d40fdee1bfa926bff45
kotet912 <kotet912@yandex.ru>
2024-10-02 23:31:44 +0300

    Создание страниц /products (Товары) /agents (Агенты) /about (О компании) /login (Авторизация)

---
+++ pages/agents.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>agents</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Products</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
