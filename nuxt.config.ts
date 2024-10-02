902cbd550298a9155e3ea6eeba78e953f5add8e5
kotet912 <kotet912@yandex.ru>
2024-10-02 13:44:14 +0300

    Установка Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', (_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      // @ts-expect-error
+      config.plugins.push(vuetify({ autoImport: true }))
+    })
+  },],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   devtools: { enabled: true }
 })
