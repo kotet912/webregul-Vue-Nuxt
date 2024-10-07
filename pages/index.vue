c685ca5ac474ff88f4e303110a2116ebc39bc61e
kotet912 <kotet912@yandex.ru>
2024-10-07 18:53:45 +0300

    работа над v-select

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,13 @@
   <v-container app grid-list-xs class="pa-0 " fluid>
     <AppCartCompany />
     <v-divider></v-divider>
-    <v-select :items="items" v-model="value" label="Все товары и услуги" variant="outlined" class="w-25 ma-auto pt-5 "
-      rounded="lg"></v-select>
+    <v-row justify="center" align="center">
+      <v-col cols="4">
+        <v-select :items="items" item-value="value" v-model="items.value" label="Все товары и услуги" variant="outlined"
+          class=" ma-auto pt-5 " rounded="lg"></v-select>
+      </v-col>
+    </v-row>
+
     <AppCart />
   </v-container>
 </template>
@@ -14,4 +19,10 @@ import AppCart from '@/components/AppCart.vue'
 definePageMeta({
   name: 'HomePage',
 })
+
+const items = [
+  { text: 'Все товары и услуги', value: 'all' },
+  { text: 'Товары', value: 'products' },
+  { text: 'Услуги', value: 'services' },
+]
 </script>
