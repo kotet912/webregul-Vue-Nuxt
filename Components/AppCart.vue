4a9a64f5c475b6ddf0af2c7706fe42d5d32b7cb5
kotet912 <kotet912@yandex.ru>
2024-10-09 00:20:43 +0300

    Доделал карточки на главной

--- Components/AppCart.vue
+++ Components/AppCart.vue
@@ -1,24 +1,47 @@
 <template>
-  <v-card>
-    <v-img :src="product.image" alt="Product Image" />
-    <v-card-title>{{ product.name }}</v-card-title>
-    <v-card-subtitle
-      >{{ product.price }} ₽ {{ product.prefix ? product.prefix : '' }}</v-card-subtitle
-    >
-    <v-card-text>
-      <p>Просмотры: {{ product.views }}</p>
-      <p>Показов: {{ product.shows }}</p>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn v-if="product.status && product.move" color="success">Объявление продвигается</v-btn>
-      <v-col v-else-if="product.status && !product.move">
-        <v-row>
-          <span>Увеличьте количество просмотров</span>
-          <v-btn color="success">Продвигать</v-btn>
-        </v-row>
+  <v-card class="position-relative" flat>
+    <v-img :src="product.image" alt="Product Image" rounded="xl" />
+    <v-row class="ga-1 position-absolute top-0 left-0 pa-5">
+      <v-chip
+        prepend-icon="mdi mdi-eye"
+        variant="flat"
+        color="rgba(27, 29, 34, 0.6)"
+        style="border-radius: 12px"
+      >
+        {{ product.views }}
+      </v-chip>
+      <v-chip
+        prepend-icon="mdi mdi-calendar-blank"
+        variant="flat"
+        color="rgba(27, 29, 34, 0.6) "
+        style="border-radius: 12px"
+      >
+        {{ product.shows }} дня
+      </v-chip>
+    </v-row>
+    <v-col class="pa-0 pt-4">
+      <v-col class="pa-0 px-1 pb-2">
+        <v-card-title class="pa-0 text-body-1" color="btnText"
+          >{{ product.price }} ₽ {{ product.prefix ? product.prefix : '' }}</v-card-title
+        >
+        <v-card-title class="pa-0 text-caption" color="btnText">{{ product.name }}</v-card-title>
       </v-col>
-      <v-btn v-else color="error">Объявление скрыто</v-btn>
-    </v-card-actions>
+      <v-divider></v-divider>
+      <v-card-actions class="pa-1 pt-2 d-flex align-center justify-center">
+        <v-btn v-if="product.status && product.move" color="bgGreen" class="text-body-2"
+          >Объявление продвигается</v-btn
+        >
+        <v-col v-else-if="product.status && !product.move">
+          <v-row align="center" justify="space-between" class="ga-3">
+            <span class="text-caption opacity-70">Увеличьте количество просмотров</span>
+            <v-btn color="bgGreen" class="text-body-2" style="border-radius: 12px" variant="flat"
+              >Продвигать</v-btn
+            >
+          </v-row>
+        </v-col>
+        <v-btn v-else color="#BD3223" class="text-body-2">Объявление скрыто</v-btn>
+      </v-card-actions>
+    </v-col>
   </v-card>
 </template>
 
