78f4ac45799534ff27c5d9768e13fee31603dcf9
kotet912 <kotet912@yandex.ru>
2024-10-13 21:46:54 +0300

    првки по PP

--- pages/AboutPage.vue
+++ pages/AboutPage.vue
@@ -3,7 +3,7 @@
     <AppCartCompany />
     <!-- TODO: отдельно поместить к компоненту чтобы не повторялся -->
     <v-divider class="hidden-sm-and-down"></v-divider>
-    <v-row class="pb-9 pb-md-16 pt-md-10 ma-0" justify="center" align="center">
+    <v-row class="pb-9 pb-md-12 pt-md-10 ma-0" justify="center" align="center">
       <v-col md="5" class="ga-3 d-flex flex-column px-0 px-md-6 pt-3 py-md-0">
         <v-card flat>
           <v-card-title class="text-h6 text-lg-h5 font-weight-bold pb-1">
@@ -31,15 +31,15 @@
           </v-card-text>
         </v-card>
         <v-col flat class="pa-0 pa-md-2">
-          <v-row class="justify-space-between align-center flex-column flex-md-row ma-0 ma-md-2">
+          <v-row class="justify-space-between flex-column flex-md-row ma-0">
             <v-col class="pa-0" order="1" order-md="0">
               <v-card-title
-                class="text-h6 text-lg-h5 custom-letter-spacing font-weight-bold pt-3 pt-md-5 pb-1"
+                class="text-h6 text-lg-h5 custom-letter-spacing font-weight-bold pt-1 pb-1 pa-md-2 pt-md-5"
               >
                 {{ aboutData.geo[0] }}
               </v-card-title>
 
-              <v-card-text>
+              <v-card-text class="pa-md-2 py-md-0">
                 <div
                   class="text-body-2 text-lg-body-1 text-bgGreen w-75 w-md-100 custom-line-height"
                 >
@@ -47,17 +47,18 @@
                 </div>
               </v-card-text>
             </v-col>
-            <v-col flat class="pa-0 mt-n1 mt-2" md="5">
+            <v-col flat class="pa-0" md="5">
               <v-img
                 :max-width="smAndDown ? '' : '200'"
                 :height="mdAndDown ? '96' : '132'"
                 :class="[
                   smAndDown ? 'br-t-20' : 'rounded-e-xl' + ' ml-auto w-auto',
-                  ' mt-md-2 ml-md-2 mr-md-n1',
+                  'mr-md-n3 mt-n1 mb-2 mt-md-0 ',
                 ]"
                 cover
                 :src="smAndDown ? aboutData.map[1] : aboutData.map[0]"
               >
+                <!-- mt-md-2 ml-md-2 mr-md-n1 -->
               </v-img>
             </v-col>
           </v-row>
